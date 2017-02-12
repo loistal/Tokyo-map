842ac5503f56dd2d1c64d2c8ae85530600f25e14
Lois Talagrand <lois.talagrand@epfl.ch>
2017-02-11 17:25:40 -1000

    Basic map

--- js/app.js
+++ js/app.js
@@ -5,5 +5,17 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
 }
