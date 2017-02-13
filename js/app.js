8d422569aaf2427b08f7ce74d9629b8c1fe9e896
Lois Talagrand <lois.talagrand@epfl.ch>
2017-02-13 08:46:10 -1000

    Add map transition when opening sidebar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("map").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("map").style.marginLeft = "0";
 }
 
 function initMap() {
