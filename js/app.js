e994d332dd9c0db617fad99b177b2905c938cb65
Lois Talagrand <lois.talagrand@epfl.ch>
2017-02-20 08:32:31 -1000

    Update 20th Feb

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+var favPlaceNames = [
+
+	"Akihabara",
+	"Asakusa",
+	"Harajuku",
+	"Shibuya",
+	"Ginza",
+	"Yodobashi-Akiba",
+	"Sensoji"
+
+];
+
+// Contains Yelp reviews of all the favorite places
+var favPlacesYelp = [
+
+
+];
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("map").style.marginLeft = "250px";
@@ -9,9 +27,9 @@ function closeNav() {
 }
 
 function initMap() {
-    var uluru = { lat: -25.363, lng: 131.044 };
+    var uluru = { lat: 35.6895, lng: 139.6917 };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 12,
         center: uluru
     });
     var marker = new google.maps.Marker({
@@ -19,3 +37,16 @@ function initMap() {
         map: map
     });
 }
+
+//***********************
+//*    VIEW MODEL       *
+//***********************
+// defines the data and behavior of the UI
+function TokyoViewModel() {
+	var self = this;
+
+	self.favPlaces = favPlacesYelp;
+}
+
+// Activates knockout.js
+ko.applyBindings(new TokyoViewModel());
