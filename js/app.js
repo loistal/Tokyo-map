849316c54e4524e71c32fe0843aa08dc8050a49d
Lois Talagrand <lois.talagrand@epfl.ch>
2017-02-20 16:01:59 -1000

    Display markers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var mMap;
+
 var mFavPlaceNames = [
 
 	"Akihabara",
@@ -6,7 +8,7 @@ var mFavPlaceNames = [
 	"Shibuya",
 	"Ginza",
 	"Yodobashi-Akiba",
-	"Sensoji"
+	"Senso-ji Temple"
 
 ];
 
@@ -17,9 +19,17 @@ var mQueryInfo = {
     "version": "20170220"
 };
 
-var mFavPlacesDetails = null;
+function httpGet(url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); 
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
 
 function getFavPlacesInfo(favPlaceNames) {
+    var favPlacesDetails = [];
+
     var i;
     for(i = 0; i < mFavPlaceNames.length; i++) {
         var url = "https://api.foursquare.com/v2/venues/search?limit=1&near=" + mQueryInfo.near 
@@ -28,10 +38,10 @@ function getFavPlacesInfo(favPlaceNames) {
             + "&client_id=" + mQueryInfo.client_id 
             + "&client_secret=" + mQueryInfo.client_secret;
 
-        $.get(url, function(data, status){
-            console.log(data);
-        });
+            favPlacesDetails.push(httpGet(url));
     }
+
+    return favPlacesDetails;
 }
 
 
@@ -46,15 +56,14 @@ function closeNav() {
 }
 
 function initMap() {
-    var uluru = { lat: 35.6895, lng: 139.6917 };
-    var map = new google.maps.Map(document.getElementById('map'), {
+
+    var center = { lat: 35.6809814, lng: 139.7538745 };
+    mMap = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
-        center: uluru
+        center: center
     });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+
+    run();
 }
 
 //***********************
@@ -72,12 +81,31 @@ function TokyoViewModel() {
 }
 
 function run() {
+    // Get details of favorite places 
+    var favPlacesDetails = getFavPlacesInfo(mFavPlaceNames);
+    var placeIndex;
+    for(placeIndex = 0; placeIndex < mFavPlaceNames.length; placeIndex++) {
+        
+        var contentString = 'Hello';
 
-    getFavPlacesInfo(mFavPlaceNames);
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var currentPlace = favPlacesDetails[placeIndex];
+        console.log(currentPlace);
+        var currentLoc = currentPlace.response.venues[0].location;
+        var marker = new google.maps.Marker({
+          position: { lat: currentLoc.lat, lng: currentLoc.lng},
+          map: mMap,
+          title: "Example"
+        });
+        marker.addListener('click', function() {
+          infowindow.open(mMap, marker);
+        });
+    }
 
     // Activates knockout.js
-    ko.applyBindings(new TokyoViewModel());
+   // ko.applyBindings(new TokyoViewModel());
 
 }
-
-run();
