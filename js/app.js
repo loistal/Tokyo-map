446794036f471e928e6e73e7a9fcd94ddbe20c9f
Lois Talagrand <lois.talagrand@epfl.ch>
2017-03-03 09:46:43 -1000

    Replace search with LIVE search and list

--- js/app.js
+++ js/app.js
@@ -82,6 +82,25 @@ function setInfoWindowContent(placeDetails, infoWindow, placeIndex, marker) {
     infoWindow.open(mMap, marker);
 }
 
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
 // Object representation of a favorite place
 var FavPlace = function(data) {
     this.name = ko.observable(data.name);
